5b0ca6b84f334f581f7df2007ca4287c16c9380d
bdeda@crownandtrident.com <bdeda@crownandtrident.com@1cd508cc-c412-11de-8745-9f8d1d7459b8>
2010-02-05 20:20:41 +0000

    initial commit

---
+++ trunk/python/animation/skeleton.py
@@ -0,0 +1,21 @@
+from bone import Bone
+
+class Skeleton:
+    """Base Skeleton class
+    
+        >>> s = Skeleton() 
+        
+        >>> repr(s)        
+        (Skeleton)
+    """
+    
+def _test():
+    import doctest
+    doctest.testmod()
+
+if __name__ == "__main__":
+    _test()
+
+    
+    
+    

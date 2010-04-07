9efd374c29324f95fd3ea2524e4a152e8c8b1b90
bdeda@crownandtrident.com <bdeda@crownandtrident.com@1cd508cc-c412-11de-8745-9f8d1d7459b8>
2010-04-07 20:24:38 +0000

    updated windows service

--- trunk/python/renderfarm/WorkerNodeService.py
+++ trunk/python/renderfarm/WorkerNodeService.py
@@ -1,52 +1,29 @@
-# originally from http://code.activestate.com/recipes/551780/
-
-from os.path import splitext, abspath
-from sys import modules
-
 import win32serviceutil
 import win32service
 import win32event
 import win32api
 
+from renderfarm.FarmManager import WorkerNodeDaemon
+import cherrypy
+
 class WorkerNodeService(win32serviceutil.ServiceFramework):
-	_svc_name_ = '_unNamed'
-	_svc_display_name_ = '_Service Template'
-	def __init__(self, *args):
-		win32serviceutil.ServiceFramework.__init__(self, *args)
-		self.log('init')
-		self.stop_event = win32event.CreateEvent(None, 0, 0, None)
-	def log(self, msg):
-		import servicemanager
-		servicemanager.LogInfoMsg(str(msg))
-        def sleep(self, sec):
-                win32api.Sleep(sec*1000, True)
+	_svc_name_ = 'VineyardWorkerNodeService'
+	_svc_display_name_ = 'Vineyard WorkerNode Service'
+	
+	def __init__(self, args):
+		win32serviceutil.ServiceFramework.__init__(self, args)
+		# self.stop_event = win32event.CreateEvent(None, 0, 0, None)
+		self.daemon = WorkerNodeDaemon()
+
 	def SvcDoRun(self):
-		self.ReportServiceStatus(win32service.SERVICE_START_PENDING)
-		try:
-			self.ReportServiceStatus(win32service.SERVICE_RUNNING)
-			self.log('start')
-			self.start()
-			self.log('wait')
-			win32event.WaitForSingleObject(self.stop_event, win32event.INFINITE)
-			self.log('done')
-		except Exception, x:
-			self.log('Exception : %s' % x)
-			self.SvcStop()
+		self.daemon.start()
+		
 	def SvcStop(self):
 		self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
-		self.log('stopping')
-		self.stop()
-		self.log('stopped')
-		win32event.SetEvent(self.stop_event)
+		self.daemon.stop()
 		self.ReportServiceStatus(win32service.SERVICE_STOPPED)
 
-	def start(self): 
-		self.running = True
-		while self.running:
-			self.sleep(10)
 
-	def stop(self): 
-		self.running = False
 
 def instart(cls, name, display_name=None, stay_alive=True):
 	'''
@@ -84,3 +61,5 @@ def instart(cls, name, display_name=None, stay_alive=True):
 		print str(x)
 
     
+if __name__ == '__main__':
+	win32serviceutil.HandleCommandLine(WorkerNodeService)

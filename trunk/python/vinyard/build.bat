237bb9b48b0a14f92dbec73ad7ef581f314ccaf4
bdeda@crownandtrident.com <bdeda@crownandtrident.com@1cd508cc-c412-11de-8745-9f8d1d7459b8>
2010-04-08 18:18:24 +0000

    updated to support installer build

--- trunk/python/vinyard/build.bat
+++ trunk/python/vinyard/build.bat
@@ -1,1 +1,22 @@
+@set PATH=C:\Program Files\NSIS;C:\Program Files (x86)\NSIS;%PATH%
+
+:clean
+rmdir /S /Q dist
+rmdir /S /Q build
+
+:pybuild
 python setup.py py2exe
+
+:addresources
+copy VinyardInstaller.nsi dist\VinyardInstaller.nsi
+copy gui\grapes.png dist\grapes.png
+copy res\header.bmp dist\header.bmp
+copy res\license.txt dist\license.txt
+copy res\startService.bat dist\startService.bat
+copy res\stopService.bat dist\stopService.bat
+copy c:\Python26\lib\site-packages\Pythonwin\mfc90.dll dist\mfc90.dll
+
+:installbuild
+makensis.exe ./dist/VinyardInstaller.nsi
+
+:end

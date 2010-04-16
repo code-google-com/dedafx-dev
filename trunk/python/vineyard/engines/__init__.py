8251598e54e35867206af56df2423afa28bf572e
bdeda@crownandtrident.com <bdeda@crownandtrident.com@1cd508cc-c412-11de-8745-9f8d1d7459b8>
2010-04-16 19:33:25 +0000

    removed engine defs, moving those to a plugin system

--- trunk/python/vineyard/engines/__init__.py
+++ trunk/python/vineyard/engines/__init__.py
@@ -1,30 +1,31 @@
-#__all__ = ["AfterEffectsEngine","3DelightEngine"]
+__all__ = ["BaseEngines"]
 
-from vineyard.engines.AfterEffectsEngine import *
-try:
-    ae_cs4_engine = AfterEffectsCS4Engine()
-except Exception, e:
-    print e
+#from vineyard.engines.AfterEffectsEngine import *
+#try:
+    #ae_cs4_engine = AfterEffectsCS4Engine()
+#except Exception, e:
+    #print e
 
-from vineyard.engines.DelightEngine import *
-try:
-    delight_engine = DelightEngine()
-except Exception, e:
-    print e
+#from vineyard.engines.DelightEngine import *
+#try:
+    #delight_engine = DelightEngine()
+#except Exception, e:
+    #print e
 
-def getEngineList():
-    ret = []
+#def getEngineList():
+    #"""this gets a list of all engines, not just locally available ones"""
+    #ret = []
     
-    try:
-        ae_cs4_engine = AfterEffectsCS4Engine(False)
-        ret.append(ae_cs4_engine)
-    except Exception, e:
-        print e
+    #try:
+        #ae_cs4_engine = AfterEffectsCS4Engine(False)
+        #ret.append(ae_cs4_engine)
+    #except Exception, e:
+        #print e
         
-    try:
-        delight_engine = DelightEngine(False)
-        ret.append(delight_engine)
-    except Exception, e:
-        print e
+    #try:
+        #delight_engine = DelightEngine(False)
+        #ret.append(delight_engine)
+    #except Exception, e:
+        #print e
     
-    return ret
+    #return ret

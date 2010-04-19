68c953032fe623901e4cf1a87a3b2d8163c8218b
bdeda@crownandtrident.com <bdeda@crownandtrident.com@1cd508cc-c412-11de-8745-9f8d1d7459b8>
2010-04-19 12:55:43 +0000

    updates to support config settings

--- trunk/python/vineyard/FarmConfig.py
+++ trunk/python/vineyard/FarmConfig.py
@@ -1,25 +1,43 @@
 #!/usr/bin/python
 
 import ConfigParser, os
-from vineyard import AUTODISCOVERY_PORT, STATUS_PORT
+import vineyard
 
 def create():
     config = ConfigParser.ConfigParser()
+    
+    config.add_section('Global')
+    config.set('Global', 'autodiscovery', vineyard.AUTODISCOVERY_ON)
+    
+    config.add_section('Manager')
+    config.set('Manager', 'status_update_period', vineyard.STATUS_UPDATE_PERIOD)
+    
+    #config.add_section('Database')
+    #config.set('Database', 'server', 'localhost')
+    
     config.add_section('Ports')
-    config.set('Ports', 'autodiscovery_port', str(AUTODISCOVERY_PORT))
-    config.set('Ports', 'status_port', str(STATUS_PORT))
+    config.set('Ports', 'autodiscovery_port', str(vineyard.AUTODISCOVERY_PORT))
+    config.set('Ports', 'status_port', str(vineyard.STATUS_PORT))
+    
     with open('vineyard.cfg', 'wb') as configfile:
         config.write(configfile)
 
     
-def load():
+def load():    
     if os.path.exists('vineyard.cfg'):
         config = ConfigParser.ConfigParser()
         config.read('vineyard.cfg')
-        AUTODISCOVERY_PORT = config.getint('Ports', 'autodiscovery_port')
-        STATUS_PORT = config.getint('Ports', 'status_port')
+        
+        vineyard.AUTODISCOVERY_ON = config.getboolean('Global', 'autodiscovery')
+        
+        vineyard.STATUS_UPDATE_PERIOD = config.getint('Manager', 'status_update_period')
+        
+        vineyard.AUTODISCOVERY_PORT = config.getint('Ports', 'autodiscovery_port')
+        vineyard.STATUS_PORT = config.getint('Ports', 'status_port')
+        
         return True
     else:
+        create()
         return False
 
 if __name__ == '__main__':
--- trunk/python/vineyard/__init__.py
+++ trunk/python/vineyard/__init__.py
@@ -1,6 +1,13 @@
 __all__ = ["FarmManager", "models", "WakeOnLan"]
 
 __version__ = "1.0.0"
+__VERSION__ = __version__
+
+# globals
+AUTODISCOVERY_ON = True
+
+#manager
+STATUS_UPDATE_PERIOD = 5
 
 # default ports
 # port to use for autodiscovery

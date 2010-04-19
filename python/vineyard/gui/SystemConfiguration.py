868e1c3fe867bdc7d296e5113c08dd1c755c5c7c
bdeda@crownandtrident.com <bdeda@crownandtrident.com@1cd508cc-c412-11de-8745-9f8d1d7459b8>
2010-04-19 14:51:23 +0000

    updated to support config data

--- python/vineyard/gui/SystemConfiguration.py
+++ python/vineyard/gui/SystemConfiguration.py
@@ -4,11 +4,15 @@ import sys, os
 from PyQt4 import QtGui, QtCore
 from vineyard import FarmConfig
 import vineyard
+from vineyard.FarmManager import NodeCache
+from vineyard import FarmConfig
 
 class SystemConfigurationDialog(QtGui.QDialog):
     
-    def __init__(self):
+    def __init__(self, nodecache=None):
         QtGui.QMainWindow.__init__(self)
+        self.nodecache = nodecache
+        
         self.resize(300, 500)
         self.center()
         self.setWindowTitle('Configuration')
@@ -20,18 +24,38 @@ class SystemConfigurationDialog(QtGui.QDialog):
         
         vbox = QtGui.QVBoxLayout()
         
-        cb = QtGui.QCheckBox("Autodiscovery", self)
-        vbox.addWidget(cb)
-        cb.setChecked(vineyard.AUTODISCOVERY_ON)
+        self.cb = QtGui.QCheckBox("Autodiscovery", self)
+        vbox.addWidget(self.cb)
+        self.cb.setChecked(vineyard.AUTODISCOVERY_ON)
         
         hbox = QtGui.QHBoxLayout()
         lbl = QtGui.QLabel("Status Update Period", self)
-        spin1 = QtGui.QSpinBox(self)
-        spin1.setMinimum(1)
-        spin1.setMaximum(1000)
-        spin1.setValue(vineyard.STATUS_UPDATE_PERIOD)
+        self.spin1 = QtGui.QSpinBox(self)
+        self.spin1.setMinimum(1)
+        self.spin1.setMaximum(1000)
+        self.spin1.setValue(vineyard.STATUS_UPDATE_PERIOD)
         hbox.addWidget(lbl)
-        hbox.addWidget(spin1)
+        hbox.addWidget(self.spin1)
+        vbox.addLayout(hbox)
+        
+        hbox = QtGui.QHBoxLayout()
+        lbl = QtGui.QLabel("Autodiscovery Port", self)
+        self.spin2 = QtGui.QSpinBox(self)
+        self.spin2.setMinimum(1024)
+        self.spin2.setMaximum(65535)
+        self.spin2.setValue(vineyard.AUTODISCOVERY_PORT)
+        hbox.addWidget(lbl)
+        hbox.addWidget(self.spin2)
+        vbox.addLayout(hbox)
+        
+        hbox = QtGui.QHBoxLayout()
+        lbl = QtGui.QLabel("Status Port", self)
+        self.spin3 = QtGui.QSpinBox(self)
+        self.spin3.setMinimum(1024)
+        self.spin3.setMaximum(65535)
+        self.spin3.setValue(vineyard.STATUS_PORT)
+        hbox.addWidget(lbl)
+        hbox.addWidget(self.spin3)
         vbox.addLayout(hbox)
         
         okCancel = QtGui.QDialogButtonBox(self)
@@ -52,7 +76,15 @@ class SystemConfigurationDialog(QtGui.QDialog):
         self.move((screen.width()-size.width())/2, (screen.height()-size.height())/2)
         
     def applyChanges(self):
-        print 'changes applied'
+        vineyard.AUTODISCOVERY_ON = self.cb.isChecked()
+        vineyard.STATUS_UPDATE_PERIOD = self.spin1.value()
+        vineyard.AUTODISCOVERY_PORT = self.spin2.value()
+        vineyard.STATUS_PORT = self.spin3.value()
+        
+        # save the data to the config file
+        FarmConfig.create()
+        # restart the node cache threads
+        self.nodecache.restart()       
         self.close()
 
 if __name__ == '__main__':    

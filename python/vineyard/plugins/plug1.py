978b752d8914944ced9043ce2d612f2352bbbc36
bdeda@crownandtrident.com <bdeda@crownandtrident.com@1cd508cc-c412-11de-8745-9f8d1d7459b8>
2010-04-19 14:51:48 +0000

    working on engine architecture

--- python/vineyard/plugins/plug1.py
+++ python/vineyard/plugins/plug1.py
@@ -1,9 +1,10 @@
 import vineyard.engines.BaseEngines 
+import inspect
 
 class AfterEffectsEngine(vineyard.engines.BaseEngines.RenderEngine):
     "After Effects Plugin"
     
     def __init__(self):
-        vineyard.engines.BaseEngines.RenderEngine.__init__(self, version="1.0", name="After Effects Engine")
+        vineyard.engines.BaseEngines.RenderEngine.__init__(self, version="1.0", name="After Effects Engine", filename=inspect.stack()[0][1])
         
 AfterEffectsEngine()

40cb71ed0cb9b40f0919d91544c6fa4ee38e2909
bdeda@crownandtrident.com <bdeda@crownandtrident.com@1cd508cc-c412-11de-8745-9f8d1d7459b8>
2010-04-21 13:59:49 +0000

    updated engines

--- python/vineyard/engines/BaseEngines.py
+++ python/vineyard/engines/BaseEngines.py
@@ -1,4 +1,4 @@
-import sys
+import sys, os
 from vineyard import FarmConfig
 
 class __EngineRegistry(object):
@@ -27,9 +27,9 @@ class __EngineRegistry(object):
 		names.append(e.name)
 	return names
     
-    def getEngineByName(self, nme):
+    def getEngineByName(self, name):
 	for e in self.__registry:
-	    if e.name == nme:
+	    if e.name == name:
 		return e
     
     def getEngine(self, name):
@@ -40,17 +40,17 @@ class __EngineRegistry(object):
     def getRegistry(self):
 	return self.__registry
     
-    def getLocalEngineDef(self, engine_name):
-	"""This function won't work properly when run as a windows service"""
-	eng = self.getEngineByName(engine_name)
-	if eng:
-	    if os.path.exists("plugins"):
-		for plugin in os.listdir("./plugins"):
-		    if plugin[-2:].lower() == 'py':
-			fn = os.path.join(os.path.abspath("./plugins"), plugin)
-			#exec(open(fn, 'r'))
-			if fn == eng.filename:
-			    return fn
+    #def getLocalEngineDef(self, engine_name):
+	#"""This function won't work properly when run as a windows service. The current directory is usually C:\windows\system32"""
+	#eng = self.getEngineByName(engine_name)
+	#if eng:
+	    #if os.path.exists("plugins"):
+		#for plugin in os.listdir("./plugins"):
+		    #if plugin[-2:].lower() == 'py':
+			#fn = os.path.join(os.path.abspath("./plugins"), plugin)
+			##exec(open(fn, 'r'))
+			#if fn == eng.filename:
+			    #return fn
 	
 EngineRegistry = __EngineRegistry()
     
@@ -62,19 +62,29 @@ class BaseEngine(object):
     To implement, derive a class from this class or a sub-class and override the buildCommand function. Most sub-classes will need to create their own isEnabled function that checks for the presence of the executable, and the buildGui function to enable users to submit a job to the Farm."""
     
     # empty command dictionary
-    _cmdFormat = []
+    _cmdFormat = {}
     __version = ''
     __cmd = ''
     __name = ''
+    __app = ''
+    # a tuple of os names that this engine can run under ie. ('nt', 'mac', 'posix')
+    __osNames = ()
+    # not sure if I'll use this, but might use platform.system() instead of os.name if I need to be more specific in the engine enable functions
+    __systemNames = ()
+    __execs = ()
     
-    def __init__(self, version="1.0", cmd="", name="Base Engine", checkEnabled=True):
+    def __init__(self, version="1.0", cmd="", name="Base Engine", 
+		 checkEnabled=True, osNames=('nt','posix','mac'),
+		 validExecutables=()):
 	self.enabled = None         
         if checkEnabled:
             if self.isEnabled():
                 self.enabled = True
+	self.__osNames = osNames
         self.__version = str(version)
         self.__cmd = str(cmd)
 	self.__name = str(name)
+	self.__execs = validExecutables
 	self.process = None
 	EngineRegistry.register(self)
 	FarmConfig.setEngineData(self.name, [("app",self.app), ("enabled",self.enabled)])
@@ -86,6 +96,14 @@ class BaseEngine(object):
         self.__version = str(version)
         
     version = property(getVersion, setVersion)
+    
+    def getExecs(self):
+	return self.__execs
+    
+    def setExecs(self, execs):
+	raise Exception, "Can only set the executables in the subclass initialization!"
+    
+    executables = property(getExecs, setExecs)
     
     def getCmd(self):
         return self.__cmd
@@ -115,8 +133,13 @@ class BaseEngine(object):
         return self.__app
     
     def setApp(self, app):
+	self.__app = ""
         if issubclass(self.__class__, BaseEngine):
-            self.__app = str(app)
+	    if type(app) == str and os.path.exists(app) and os.path.split(app)[1] in self.__execs:
+		app = os.path.abs
+		self.__app = str(app)
+	    else:
+		raise Exception, "Invalid app! " + str(app) + "  Valid apps are:" + str(self.__execs)
         else:
             raise Exception, "Only derived classes can set the application for the engine!"
     
@@ -126,8 +149,8 @@ class BaseEngine(object):
 	return self._cmdFormat
     
     def setCmdFormat(self, cmdFormat):
-	if issubclass(self.__class__, BaseEngine):
-            self._cmdFormat = str(cmdFormat)
+	if issubclass(self.__class__, BaseEngine) and type(cmdFormat) == dict:
+            self._cmdFormat = cmdFormat
         else:
             raise Exception, "Only derived classes can set the command dictionary for the engine!"
 	
@@ -152,8 +175,35 @@ class BaseEngine(object):
         raise NotImplementedError('must be implemented in the derived classes')
     
     def isEnabled(self, force_check=False):
-        """should be implemented in the derived classes"""
-        return True
+        """should be implemented in the derived classes.
+	Sub-class should call this base method first, so basic functionality can be checked, such as the configuration file settings."""
+        if self.enabled != None and not force_check:
+            return self.enabled
+	else:
+	    # forcing...sanity check
+	    self.enabled = None 
+        
+        if os.name not in self.__osNames:
+	    self.enabled = False
+            return self.enabled
+        
+        # check the config file first!
+        _data = FarmConfig.getEngineData(self.name)
+	if len(_data) >= 2:
+	    try:
+		for (name, val) in _data:
+		    if name == 'app':
+			self.app = val
+		    if name == 'enabled' and type(val) == bool:
+			self.enabled = val
+	    except: pass
+	self.enabled = None
+	return self.enabled
+    
+    def commitConfig(self):
+	"""call this in a subclass isEnabled function after the engine is set up, so the info is cached in the ini file"""
+	FarmConfig.setEngineData( self.name, [('app',self.app),('enabled',self.enabled)] )
+	
     
     def kill(self):
         """may be implemented in the derived classes"""
--- python/vineyard/plugins/MayaEngine.py
+++ python/vineyard/plugins/MayaEngine.py
@@ -1,44 +1,79 @@
-import subprocess, os, sys, unittest, time
+import os
 from vineyard.engines.BaseEngines import *
 
 class Maya2010Engine(RenderEngine):
        
     def __init__(self):
-        RenderEngine.__init__(self, version="1.0", name="Maya 2010 Engine")          
+        RenderEngine.__init__(self, 
+                              version="1.0", 
+                              name="Maya 2010 Engine",
+                              osNames=('nt'),# (TODO:) 'posix', 'mac'
+                              validExecutables=("Render.exe")
+                              )
+        self.commandFormat = {'filename':'',
+                              }     
+        self.renderers = ()
    
     def buildCommand(self, kwargs):
+        """Build the command-line command to execute in order to do the rendering"""
         self.command = ""
+        try:
+            if not self.isEnabled():
+                return
+        except Exception, e:
+            print "<ERROR>", e
+            return
+        self.command = self.app
+        
+        
+        
+        # filename should be last in the command, so iterate again
+        for key in kwargs:
+            if key == 'filename':
+                if type(kwargs[key]) == str:
+                    f = kwargs[key]
+                    if os.path.exists(f):
+                        self.command += " " + str(f)
+                    else:
+                        self.command = ""
+                        raise Exception, "File does not exist!"
+                else:
+                    self.command = ""
+                    raise Exception, "File needs to be a string."
         
     def isEnabled(self, force_check=False):
-        if self.enabled != None and not force_check:
-            return self.enabled
+        ret = RenderEngine.isEnabled(self, force_check)
         
-        if os.name not in ('nt','mac','posix'):
-            return False
-        
-        # check the config file first!
-        _data = FarmConfig.getEngineData(self.name)
-        
-        if len(_data) == 0:
-            pass
-        else:
-            bRet = False
-            for (name, value) in _data:
-                if name == 'app':
-                    pass
-                    #if os.path.exists(value):
-                        #if len(value) > 12 and value[-10:] == "render.exe":
-                            #self.app = value
-                            #self.enabled = True
-                        #else:
-                            #raise Exception, "App path of " + str(self.name) + " is invalid: " + str(value) 
-                    #else:
-                        #raise Exception, "App path of " + str(self.name) + " does not exist: " + str(value)
-                if name == 'enabled':
-                    self.enabled = bool(value)
-                    bRet = True
-            if bRet:
-                return self.enabled
-        
-        return False
+        if ret == None:
+            self.enabled = False
+            self.app = ''
+            if os.name == 'nt':                
+                try:
+                    aReg = ConnectRegistry(None,HKEY_LOCAL_MACHINE)
+                    aKey = OpenKey(aReg, r"SOFTWARE\Autodesk\Maya\2010\Setup\InstallPath") 
+                    for i in range(1024):
+                        val = EnumValue(aKey, i)
+                        pth = str(val[1])
+                        if val[0] == 'MAYA_INSTALL_LOCATION' and os.path.isdir(pth):
+                            pth = os.path.join(pth, 'bin')
+                            if os.path.isdir(pth):
+                                # it's installed, get the full app path and return True
+                                for app in self.executables:
+                                    a = os.path.join(pth, app)
+                                    if os.path.exists(a):
+                                        self.app = a
+                                        self.enabled = True
+                                        break
+                except Exception, e:
+                    self.enabled = False
+                    
+        # get the available renderers installed in this maya installation
+        if self.enabled:
+            self.renderers = ()
+            import subprocess
+            proc = subprocess.Popen([self.app, " -listRenderers"])
+            print retcode
+                    
+        self.commitConfig()
+        return self.enabled
         

56bb3333ae9196edfc0b45d61ca5efb82c804f81
bdeda@crownandtrident.com <bdeda@crownandtrident.com@1cd508cc-c412-11de-8745-9f8d1d7459b8>
2010-04-30 19:19:23 +0000

    fixed a formatting error in svcstop

--- python/vineyard/VineyardService.py
+++ python/vineyard/VineyardService.py
@@ -1,30 +1,37 @@
 import win32serviceutil
 import win32service
 import win32event
+import servicemanager
 
 from vineyard.FarmManager import WorkerNodeDaemon
 import cherrypy
 
 class VinyardService(win32serviceutil.ServiceFramework):
-	_svc_name_ = 'VineyardService'
-	_svc_display_name_ = 'Vineyard Service'
+    _svc_name_ = 'VineyardService'
+    _svc_display_name_ = 'Vineyard Service'
 	
-	def __init__(self, args):
-		win32serviceutil.ServiceFramework.__init__(self, args)
-		self.stop_event = win32event.CreateEvent(None, 0, 0, None)
-		self.daemon = WorkerNodeDaemon()
+    def __init__(self, args):
+	win32serviceutil.ServiceFramework.__init__(self, args)
+	self.stop_event = win32event.CreateEvent(None, 0, 0, None)
+	self.daemon = WorkerNodeDaemon()
 
-	def SvcDoRun(self):
-		self.daemon.start()
-		if cherrypy.__version__[:3] == '3.0' or cherrypy.__version__[0] == '2':
-			win32event.WaitForSingleObject(self.stop_event, win32event.INFINITE)
+    def SvcDoRun(self):
+	try:
+	    self.daemon.start()
+	    servicemanager.LogInfoMsg("Vineyard service started successfully")
+	
+
+	    if cherrypy.__version__[:3] == '3.0' or cherrypy.__version__[0] == '2':
+		win32event.WaitForSingleObject(self.stop_event, win32event.INFINITE)
+	except Exception, e:
+	    servicemanager.LogInfoMsg("Vineyard service failed. " + str(e))
 
 		
-	def SvcStop(self):
-		self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
-		self.daemon.stop()
-		win32event.SetEvent(self.stop_event)
-		self.ReportServiceStatus(win32service.SERVICE_STOPPED)
+    def SvcStop(self):
+	self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
+	self.daemon.stop()
+	win32event.SetEvent(self.stop_event)
+	self.ReportServiceStatus(win32service.SERVICE_STOPPED)
     
 if __name__ == '__main__':
 	win32serviceutil.HandleCommandLine(VinyardService)
